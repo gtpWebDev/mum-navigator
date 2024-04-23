bf48699f386c1bdc0aa42114ec9d193c6bb765eb
gtpWebDev <33732289+gtpWebDev@users.noreply.github.com>
2024-04-23 13:49:30 +0100

    Small prescription update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import './styles.css';
 import { format, formatDistanceToNowStrict } from 'date-fns';
 
-const latestPrescriptionDate = new Date('April 13, 2024');
-
-const result = formatDistanceToNowStrict(latestPrescriptionDate, {
-  addSuffix: true,
-});
-
-const friendlyDate = format(latestPrescriptionDate, 'do MMMM');
-
-const daysAgoText = `The last prescription was sent ${result} (${friendlyDate})`;
+// 1. UPDATE WHETHER TO SHOW RECEIVED OR ORDERED - USE MOST RECENT
+const showOrdered = false;
+// 2. UPDATE ORDERED DATE HERE IF RELEVANT
+const latestOrderedDate = new Date('April 13, 2024');
+// 3. UPDATE RECEIVED DATE HERE IF RELEVANT
+const latestReceivedDate = new Date('April 23, 2024');
 
 const latestPrescriptionText = document.querySelector(
   '#latest-prescription-text'
 );
-latestPrescriptionText.textContent = daysAgoText;
+let constructedText = '';
+
+if (showOrdered) {
+  const orderedDaysAgo = formatDistanceToNowStrict(latestOrderedDate, {
+    addSuffix: true,
+  });
+  const friendlyOrderedDate = format(latestOrderedDate, 'do MMMM');
+  constructedText = `The last prescription was ordered ${orderedDaysAgo} (${friendlyOrderedDate}). It has not arrived yet.`;
+} else {
+  const receivedDaysAgo = formatDistanceToNowStrict(latestReceivedDate, {
+    addSuffix: true,
+  });
+  const friendlyReceivedDate = format(latestReceivedDate, 'do MMMM');
+  constructedText = `The last prescription arrived ${receivedDaysAgo} (${friendlyReceivedDate}). You have not ordered a new prescription since then.`;
+}
+
+latestPrescriptionText.textContent = constructedText;
